58dd9a9381b88975f415dfe19d20d42dd0918995
Leoozito <leonardonogueira616@gmail.com>
2024-05-20 19:46:26 -0300

    Adding pagination, back end sent total page number to do pagination on the front

---
+++ front/src/components/Pagination/index.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import { usePagination } from './usePagination'
+
+type PaginationProps = { page: number, limit: number, total: number }
+
+const Pagination = ({ page, limit, total }: PaginationProps) => {
+    
+    const {pages} = usePagination({page, limit, total})
+    
+    return (
+        <>
+            <nav aria-label="Page navigation example">
+                <ul className="inline-flex -space-x-px text-base h-10">
+                    <li>
+                        <Link href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</Link>
+                    </li>
+                    <li>
+                        <Link href="#" aria-current="page" className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</Link>
+                    </li>
+                    <li>
+                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Pagination
