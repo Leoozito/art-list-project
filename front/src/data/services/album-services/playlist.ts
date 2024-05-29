315846a42a8947cf027cc3f53bcd9820e750577f
Leoozito <leonardonogueira616@gmail.com>
2024-05-29 13:54:07 -0300

    removing login bug

--- front/src/data/services/album-services/playlist.ts
+++ front/src/data/services/album-services/playlist.ts
@@ -3,12 +3,13 @@ import { fetchWrapper } from '../../../app/functions/fetch'
 interface AlbumProps {
     nameAlbum: string,
     yearAlbum: string,
-    // artist: string,
-    // user_id : number
+    artist: string,
+    user_id : number
 }
 
 export default async function newAlbumService(albumDatas: AlbumProps) 
 {
+    // console.log("ALBUMM TESTE:", albumDatas)
     const response = await fetchWrapper('/albums/create', {
         method: 'POST',                       
         headers: {
