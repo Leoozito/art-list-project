b9af8c4066efebe5be664904a1669bd75e033cf3
Leoozito <leonardonogueira616@gmail.com>
2024-06-03 00:27:13 -0300

    Completing the pagination of the "playlist" screen

--- front/src/components/Pagination/index.tsx
+++ front/src/components/Pagination/index.tsx
@@ -1,37 +1,62 @@
+"use client"
+
 import Link from 'next/link'
-import { usePagination } from './usePagination'
+import { ELLIPSIS_LEFT, ELLIPSIS_RIGHT, usePagination } from './usePagination'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 type PaginationProps = { page: number, limit: number, total: number }
 
 const Pagination = ({ page, limit, total }: PaginationProps) => {
     
-    const {pages} = usePagination({page, limit, total})
-    
+    const { pages, isCurrentPage } = usePagination({page, limit, total})
+
+    const pathName = usePathname()
+    const searchParams = useSearchParams()
+
+    const generateUrl = (page:number) => {
+        const params = new URLSearchParams(searchParams)
+        params.set('page', page.toString());
+
+        const url = `${pathName}?${params.toString()}`
+        return url
+    }
+
+    const className = "flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+
     return (
         <>
             <nav aria-label="Page navigation example">
                 <ul className="inline-flex -space-x-px text-base h-10">
-                    <li>
-                        <Link href="#" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</Link>
-                    </li>
-                    <li>
-                        <Link href="#" aria-current="page" className="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</Link>
-                    </li>
+                    {pages.map((page, index) => {
+
+                        const isEllipsis = page == ELLIPSIS_LEFT || page == ELLIPSIS_RIGHT;
+
+                        const isFirst = index == 0; 
+                        const isLast = index + 1 == pages.length;
+                        
+                        const isCurrent = isCurrentPage(page)
+
+                        {isEllipsis &&(
+                            <li
+                                key={page}
+                            >
+                                <span className={className}> ... </span>
+                            </li>
+                        )}
+
+                        return(
+                            <li
+                                key={page}
+                            >
+                                <Link 
+                                    href={generateUrl(page)} 
+                                    className={className}
+                                >
+                                    {page}
+                                </Link>
+                            </li>
+                        )
+                    })}    
                 </ul>
             </nav>
         </>
