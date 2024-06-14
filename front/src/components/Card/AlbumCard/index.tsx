0b05646f22a53abd6d5a202b7f54fd33e828ac78
Leoozito <leonardonogueira616@gmail.com>
2024-06-14 15:12:17 -0300

    Typing component albumCard

--- front/src/components/Card/AlbumCard/index.tsx
+++ front/src/components/Card/AlbumCard/index.tsx
@@ -8,7 +8,16 @@ interface AlbumDatas {
     year_album: string 
 }
 
-const AlbumCard = ({isAdm, key, onClick,}:any, { artist, name_album, year_album }:AlbumDatas) => {
+interface AlbumCardProps {
+    albumDatas: AlbumDatas;
+    key: string | number;
+    onClick: () => void;
+    isAdm: boolean;
+}
+
+const AlbumCard: React.FC<AlbumCardProps> = ({ albumDatas }, {key, onClick, isAdm}:any) => {
+
+    console.log("dados: ", albumDatas)
 
     return(
         <>
@@ -20,9 +29,9 @@ const AlbumCard = ({isAdm, key, onClick,}:any, { artist, name_album, year_album 
                 <div className="px-8 sm:py-4 lg:py-0 lg:flex lg:flex-row sm:justify-center sm:flex-col items-center">
                     <PiMusicNotesFill className="text-6xl text-orange-700 text-center font-extrabold mr-8" />
                     <div className="flex flex-col sm:justify-center lg:justify-between p-4 leading-normal sm:text-lg">
-                        <a className="mb-4 text-2xl font-bold tracking-tight text-orange-600 dark:text-white capitalize">Album name: {name_album}</a>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Artist: {artist}</p>
-                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Year of release: {year_album}</p>
+                        <a className="mb-4 text-2xl font-bold tracking-tight text-orange-600 dark:text-white capitalize">Album name: {albumDatas.name_album}</a>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Artist: {albumDatas.artist}</p>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Year of release: {albumDatas.year_album}</p>
                     </div>
                     {isAdm && (
                         <MdOutlineDeleteForever 
